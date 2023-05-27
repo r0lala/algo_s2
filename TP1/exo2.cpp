7582f5d9330f95182987ce3ea351f933183a2081
rolala <romane.morice77@gmail.com>
2023-05-27 14:30:11 +0200

    fibonacci (ok)

--- TP1/exo2.cpp
+++ TP1/exo2.cpp
@@ -9,8 +9,13 @@ int fibonacci(int value)
     Context _("fibonacci", value); // do not care about this, it allow the display of call stack
 
     // your code
-
-    return_and_display(value);
+    if(value==0){
+        return 0;
+    }if (value==1){
+        return 1;
+    }else{
+        return_and_display(fibonacci(value-1) + fibonacci(value-2));
+    }
 }
 
 int main(int argc, char *argv[])
