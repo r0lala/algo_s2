9d745e4ef4ccb0b5ecd4447ad93363c0a355fe33
rolala <romane.morice77@gmail.com>
2023-05-27 16:01:24 +0200

    maj TP1 exo6 (Liste)

--- TP1/exo6.cpp
+++ TP1/exo6.cpp
@@ -4,118 +4,145 @@ using namespace std;
 
 struct Noeud{
     int donnee;
-    Noeud* suivant;
+    Noeud* next;
 };
 
 struct Liste{
-    Noeud* premier;
-    // your code
+    Noeud* first;
+    Noeud* last;
+    int size;
 };
 
 struct DynaTableau{
     int* donnees;
-    // your code
+    int size;
+    
 };
 
 
-void initialise(Liste* liste)
-{
-
+void initialise(Liste* liste){
+    liste->first=nullptr;
 }
 
-bool est_vide(const Liste* liste)
-{
-    return false;
+bool est_vide(const Liste* liste){
+    if(liste->first==nullptr){
+        return true;
+    }else{
+        return false;
+    }
 }
 
-void ajoute(Liste* liste, int valeur)
-{
-
+void ajoute(Liste* liste, int valeur){
+    Noeud *new_n = new Noeud; //création nouveau noeud
+    new_n->donnee=valeur;     //valeur du nouveau noeud
+    new_n->next=nullptr;      //pas de noeud après lui
+    //si pas de premier noeud existant
+    if(liste->first==nullptr){
+        liste->first= new_n; // alors le nouveau est le premier 
+        liste->last=new_n;   //et le dernier pcq il est tout seul
+    }//sinon (donc si y a au moins un noeud dans liste)
+    else{
+        liste->last->next=new_n; // le noeud après le dernier devient le nouveau noeud
+        liste->last=liste->last->next; // et ce noeud devient le dernier de la liste
+    }
 }
 
-void affiche(const Liste* liste)
-{
-
+void affiche(const Liste* liste){
+    if(liste->first==nullptr){  //pas de noeud dans la liste
+        cout<<"La liste est vide"<<endl; 
+    }else{
+        Noeud* noeud=liste->first;
+        while(noeud->next!=nullptr){ // tant qu'il y a un noeud après
+            cout<<"Noeud : "<<noeud<<" || valeur : "<<noeud->donnee<<endl; // on affiche
+            noeud=noeud->next;
+        }
+    }
 }
 
-int recupere(const Liste* liste, int n)
-{
-    return 0;
+int recupere(const Liste* liste, int n){
+    if(n==0 || n>liste->size){
+        return -1;
+    }else{
+        Noeud* noeud = liste->first;
+
+        while(int i=0 ; i<n-1 ; i++){
+            noeud=noeud->next;
+        }
+        return noeud->donnee;
+    }
 }
 
-int cherche(const Liste* liste, int valeur)
-{
+int cherche(const Liste* liste, int valeur){
+    Noeud* noeud=liste->first;
+    for(int i=0 ; noeud!=nullptr ; i++){
+        if(noeud->donnee==valeur){
+            return i;
+        }
+        noeud=noeud->next;
+    }
     return -1;
 }
 
-void stocke(Liste* liste, int n, int valeur)
-{
+void stocke(Liste* liste, int n, int valeur){
+    Noeud* noeud=liste->first;
+    int i=0;
+    while(i<n){
+        noeud=noeud->next;
+    }
+    noeud->donnee=valeur;
+}
+
+void ajoute(DynaTableau* tableau, int valeur){
 
 }
 
-void ajoute(DynaTableau* tableau, int valeur)
-{
+
+void initialise(DynaTableau* tableau, int capacite){
 
 }
 
-
-void initialise(DynaTableau* tableau, int capacite)
-{
-
-}
-
-bool est_vide(const DynaTableau* liste)
-{
+bool est_vide(const DynaTableau* liste){
     return false;
 }
 
-void affiche(const DynaTableau* tableau)
-{
+void affiche(const DynaTableau* tableau){
 
 }
 
-int recupere(const DynaTableau* tableau, int n)
-{
+int recupere(const DynaTableau* tableau, int n){
     return 0;
 }
 
-int cherche(const DynaTableau* tableau, int valeur)
-{
+int cherche(const DynaTableau* tableau, int valeur){
     return -1;
 }
 
-void stocke(DynaTableau* tableau, int n, int valeur)
-{
+void stocke(DynaTableau* tableau, int n, int valeur){
 
 }
 
 //void pousse_file(DynaTableau* liste, int valeur)
-void pousse_file(Liste* liste, int valeur)
-{
+void pousse_file(Liste* liste, int valeur){
 
 }
 
 //int retire_file(Liste* liste)
-int retire_file(Liste* liste)
-{
+int retire_file(Liste* liste){
     return 0;
 }
 
 //void pousse_pile(DynaTableau* liste, int valeur)
-void pousse_pile(Liste* liste, int valeur)
-{
+void pousse_pile(Liste* liste, int valeur){
 
 }
 
 //int retire_pile(DynaTableau* liste)
-int retire_pile(Liste* liste)
-{
+int retire_pile(Liste* liste){
     return 0;
 }
 
 
-int main()
-{
+int main(){
     Liste liste;
     initialise(&liste);
     DynaTableau tableau;
