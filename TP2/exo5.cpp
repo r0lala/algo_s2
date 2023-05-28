116e248bfaf8ece96e223664e902040ac7af4b68
rolala <romane.morice77@gmail.com>
2023-05-28 10:54:20 +0200

    TP2 - merge

--- TP2/exo5.cpp
+++ TP2/exo5.cpp
@@ -9,21 +9,57 @@ void merge(Array& first, Array& second, Array& result);
 void splitAndMerge(Array& origin)
 {
 	// stop statement = condition + return (return stop the function even if it does not return anything)
+	if(origin.size()<=1){
+		return;
+	}
 
 	// initialisation
 	Array& first = w->newArray(origin.size()/2);
 	Array& second = w->newArray(origin.size()-first.size());
 	
 	// split
-
+	for(int i=0 ; i<first.size() ; i++){
+		first.get(i)=origin.get(i); // identique à first[i]=origin[i]
+	}
+	for(int j=0 ; j<second.size() ; j++){
+		second[j]=origin[first.size()+1];
+	}
 	// recursiv splitAndMerge of lowerArray and greaterArray
+	splitAndMerge(first);
+	splitAndMerge(second);
 
 	// merge
+	merge(first, second, origin);
 }
 
 void merge(Array& first, Array& second, Array& result)
 {
+	int i=0;
+	int firstI=0;
+	int secondI=0;
 
+	while(firstI<first.size() && secondI<second.size()){
+		if(first[firstI] <= second[secondI]){
+			result[i]=first[firstI];
+			firstI++;
+			i++;
+		}else{
+			result[i]=second[secondI];
+			secondI++;
+			i++;
+		}
+	}
+	while(first<first.size()){
+		result[i]=first[firstI];
+		firstI++;
+		i++;
+
+	}
+	while(secondI<second.size()){
+		result[i]=second[secondI];
+		secondI++;
+		i++;
+	}
 }
 
 void mergeSort(Array& toSort)
