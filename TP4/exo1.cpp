e7587876a391b7fa7906c9db400b3300439652ec
rolala <romane.morice77@gmail.com>
2023-05-28 09:36:33 +0200

    TP4[exo1] - Finito

--- TP4/exo1.cpp
+++ TP4/exo1.cpp
@@ -9,26 +9,22 @@ MainWindow *w = nullptr;
 using std::size_t;
 using std::string;
 
-int Heap::leftChild(int nodeIndex)
-{
+int Heap::leftChild(int nodeIndex){
 	int child_g = nodeIndex * 2 + 1;
 	return child_g;
 }
 
-int Heap::rightChild(int nodeIndex)
-{
+int Heap::rightChild(int nodeIndex){
 	int child_d = nodeIndex * 2 + 2;
 	return child_d;
 }
 
-void Heap::insertHeapNode(int heapSize, int value)
-{
+void Heap::insertHeapNode(int heapSize, int value){
 	// use (*this)[i] or this->get(i) to get a value at index i
 	int i = heapSize;
 
 	this->get(i) = value;
-	while (i > 0 && this->get(i) > this->get((i - 2) / 2))
-	{
+	while (i > 0 && this->get(i) > this->get((i - 2) / 2)){
 		this->swap(i, (i - 1) / 2);
 		i = (i - 2) / 2;
 	}
@@ -36,8 +32,7 @@ void Heap::insertHeapNode(int heapSize, int value)
 
 //Si le noeud à l’indice nodeIndex n’est pas supérieur
 //à ses enfants, reconstruit le tas à partir de cet index.
-void Heap::heapify(int heapSize, int nodeIndex)
-{
+void Heap::heapify(int heapSize, int nodeIndex){
 	// use (*this)[i] or this->get(i) to get a value at index i
 	int i_max = nodeIndex;
 
@@ -56,22 +51,19 @@ void Heap::heapify(int heapSize, int nodeIndex)
 
 //Construit un tas à partir des valeurs de numbers (vous pouvez
 //utiliser soit insertHeapNode soit heapify)
-void Heap::buildHeap(Array &numbers)
-{
+void Heap::buildHeap(Array &numbers){
 	for(int i=0; i<numbers.size() ; i++){
 		this->insertHeapNode(i, numbers[i]);
 	}
 }
 
-void Heap::heapSort()
-{
+void Heap::heapSort(){
 	int size = this->size()-1;
 	// ne pas mettre le -1 directement dans les conditions du for()
 	for(int i=size; i>0 ; i--){
 		this->swap(0,i);
 		this->heapify(i,0);
 	}
-
 }
 
 int main(int argc, char *argv[])
